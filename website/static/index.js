04257c033866eade5bb4fa0c04b7060a49856356
Shreyanshukunwar <shreyanshukunwar@gmail.com>
2021-06-23 15:22:51 +0545

    Note add and delete

---
+++ website/static/index.js
@@ -0,0 +1,11 @@
+function deleteNote(noteId){
+	fetch('/delete-note', {
+		method: 'POST',
+		body: JSON.stringify({
+			noteId: noteId
+		})
+	})
+	.then(resp => {
+		window.location.href = "/";
+	});
+}
--- website/views.py
+++ website/views.py
@@ -1,11 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, request, flash, jsonify
 from flask import render_template
 
 from flask_login import login_required, current_user
+from .models import Note
+from .import db
+
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+
+	if request.method == 'POST':
+		note = request.form.get('note')
+		if len(note) <= 1:
+			flash('Note is too short', category='error')
+		else:
+			new_note = Note(data=note, user_id=current_user.id)
+			db.session.add(new_note)
+			db.session.commit()
+			flash('Note added successfully', category='success')
 	return render_template('home.html', user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+	data = json.loads(request.data)
+	noteId = data['noteId']
+	note = Note.query.get(noteId)
+
+	if note:
+		if note.user_id == current_user.id:
+			db.session.delete(note)
+			db.session.commit()
+	return jsonify({})
